fe19eeca217cbeab3f257246bb1e70bb7f12bb0e
yaohua.cyh <yaohua.cyh@alibaba-inc.com>
2016-08-31 11:43:06 +0800

    add files

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# YohaAnimation
+Description:
+			一款轻量级js高性能dom动画库，原生js编写。
+			对浏览器重绘以及回流做了很多优化，保证了dom操作的高性能。
+			动画很流畅，并发动画表现很好，兼容ie6+。
+使用方法:
+      参数:
+          element : dom节点
+          targetStyle: dom的动画结束状态  
+                       例如，{ left:'100px',width:'100px',opacity:'0.5',scrollTop:1000}，其它可自由发挥
+          duration: 动画持续时长，毫秒单位, 如 1000
+          callback: 动画结束的回调函数，可用于做连续动画。
+      API：
+		  Yoha.aniamte(element,targetStyle,duration).done(callback);
+		  Yoha.show(element, duration)
+		  Yoha.hide(element, duration)
+		  Yoha.slideDown(element, duration)
+		  Yoha.slideUp(element, duration)
---
+++ js/yoha-animation.js
@@ -0,0 +1,283 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	window.Yoha = window.Yoha || {};
+	var $animateQueue = [];
+	var frame_duration = requestAnimationFrame ? 4 : 16;
+	Array.prototype.map = Array.prototype.map || function (cb) {
+	    for (var i = 0; i < this.length; i++) {
+	        cb(this[i], i);
+	    }
+	};
+	Array.prototype.indexOf = Array.prototype.indexOf || function (item) {
+	    for (var i = 0; i < this.length; i++) {
+	        if (this[i] == item) return i;
+	    }
+	    return -1;
+	};
+	var isEmptyObj = function isEmptyObj(obj) {
+	    for (var i in obj) {
+	        return false;
+	    }
+	    return true;
+	};
+	var requestAnimationFrame = requestAnimationFrame || function (action) {
+	    setTimeout(function () {
+	        action();
+	    }, frame_duration);
+	};
+	Yoha.animate = function (el, option, duration, cb) {
+	    var empty = {};
+	    var style = el.style;
+	    var css_style = window.getComputedStyle ? window.getComputedStyle(el, null) : el.currentStyle;
+	    var animate_task = {
+	        el: el,
+	        style: style,
+	        duration: duration,
+	        option: option,
+	        frame: "",
+	        cb: ""
+	    };
+	    var frame = {};
+	    for (var key in option) {
+	        var default_value = style[key] || css_style[key] || el[key];
+
+	        if (default_value == 'auto') {
+	            switch (key) {
+	                case 'height':
+	                    default_value = el.offsetHeight + 'px';
+	                    break;
+	                case 'width':
+	                    default_value = el.offsetWidth + 'px';
+	                    break;
+	                default:
+	                    default_value = '0px';
+	                    break;
+	            }
+	        }
+
+	        if (default_value == option[key]) {
+	            delete option[key];
+	            continue;
+	        }
+
+	        if (!default_value) {
+	            switch (key) {
+	                case 'opacity':
+	                    default_value = 1;
+	                    break;
+	                default:
+	                    default_value = 0;
+	                    break;
+	            }
+	        }
+	        var difference = parseFloat(option[key]) - parseFloat(default_value);
+	        frame[key] = difference / (duration / frame_duration);
+	        animate_task[key] = default_value;
+	    }
+	    animate_task.frame = frame;
+	    if (!isEmptyObj(option)) {
+	        $animateQueue.push(animate_task);
+	    }
+	    empty.done = function (cb) {
+	        if (isEmptyObj(option)) cb && cb.call(el);else animate_task.cb = cb;
+	    };
+	    return empty;
+	};
+
+	Yoha.show = function (el, duration, cb) {
+	    var style = el.style;
+	    var init_style = window.getComputedStyle ? window.getComputedStyle(el) : el.currentStyle;
+
+	    var default_style = {
+	        height: init_style.height,
+	        width: init_style.width
+	    };
+
+	    if (init_style.display != 'none') return;
+
+	    style.display = 'block';
+
+	    // var css_style = window.getComputedStyle ? window.getComputedStyle(el) : el.currentStyle;
+
+	    var target_style = {
+	        height: el.offsetHeight + 'px',
+	        width: el.offsetWidth + 'px'
+	    };
+
+	    style.width = '0px';
+	    style.height = '0px';
+
+	    Yoha.animate(el, { width: target_style.width, height: target_style.height }, duration).done(function () {
+	        style.height = default_style.height;
+	        style.width = default_style.width;
+	        cb && cb.call(el);
+	    });
+	};
+	Yoha.hide = function (el, duration, cb) {
+	    var postfix_reg = /[\d\.]+([^0-9\.]*)/;
+	    var style = el.style;
+	    var css_style = window.getComputedStyle ? window.getComputedStyle(el) : el.currentStyle;
+	    if (style.display == 'none' || css_style.display == 'none') return;
+
+	    var default_style = {
+	        height: style.height || css_style.height,
+	        width: style.width || css_style.width
+	    };
+
+	    var height_postfix = default_style.height != 'auto' ? postfix_reg.exec(default_style.height)[1] : 'px';
+	    var width_postfix = default_style.width != 'auto' ? postfix_reg.exec(default_style.width)[1] : 'px';
+
+	    Yoha.animate(el, { width: 0 + width_postfix, height: 0 + height_postfix }, duration).done(function () {
+	        style.display = 'none';
+	        style.height = default_style.height;
+	        style.width = default_style.width;
+	        cb && cb.call(el);
+	    });
+	};
+	Yoha.slideDown = function (el, duration, cb) {
+	    var style = el.style;
+	    var init_style = window.getComputedStyle ? window.getComputedStyle(el) : el.currentStyle;
+
+	    var default_style = {
+	        height: init_style.height
+	    };
+
+	    if (init_style.display != 'none') return;
+
+	    style.display = 'block';
+
+	    // var css_style = window.getComputedStyle ? window.getComputedStyle(el) : el.currentStyle;
+
+	    var target_style = {
+	        height: el.offsetHeight + 'px'
+	    };
+
+	    style.height = '0px';
+
+	    Yoha.animate(el, { height: target_style.height }, duration).done(function () {
+	        style.height = default_style.height;
+	        cb && cb.call(el);
+	    });
+	};
+	Yoha.slideUp = function (el, duration, cb) {
+	    var postfix_reg = /[\d\.]+([^0-9\.]*)/;
+	    var style = el.style;
+	    var css_style = window.getComputedStyle ? window.getComputedStyle(el) : el.currentStyle;
+	    if (style.display == 'none' || css_style.display == 'none') return;
+
+	    var default_style = {
+	        height: style.height || css_style.height
+	    };
+
+	    var height_postfix = default_style.height != 'auto' ? postfix_reg.exec(default_style.height)[1] : 'px';
+	    Yoha.animate(el, { height: 0 + height_postfix }, duration).done(function () {
+	        style.display = 'none';
+	        style.height = default_style.height;
+	        cb && cb.call(el);
+	    });
+	};
+
+	function animate_start() {
+	    var last_time;
+	    var current_time;
+	    var window_styles = ['scrollTop'];
+	    var update = function animate_start() {
+	        var postfix_reg = /[\d\.]+([^0-9\.]*)/;
+	        current_time = new Date().getTime();
+	        $animateQueue.map(function (task, index) {
+	            var style = task.style;
+	            var frame = task.frame;
+	            var option = task.option;
+	            var cb = task.cb;
+	            var el = task.el;
+	            for (var key in option) {
+	                var pattern = parseFloat(task[key]);
+	                var postfix = postfix_reg.exec(option[key])[1];
+	                var frame_value = frame[key];
+	                if (last_time) {
+	                    frame_value = (current_time - last_time) / frame_duration * frame[key];
+	                }
+	                if (Math.abs(frame[key]) > Math.abs(pattern - parseFloat(option[key]))) {
+	                    for (var _key in option) {
+	                        if (window_styles.indexOf(_key) < 0) style[_key] = option[_key];else el[_key] = option[_key];
+	                    }
+	                    $animateQueue.splice(index, 1);
+	                    cb && cb.call(el);
+	                    break;
+	                } else {
+	                    pattern = pattern + frame[key] + postfix;
+	                    if (window_styles.indexOf(key) < 0) style[key] = task[key] = pattern;else el[key] = task[key] = pattern;
+	                }
+	            }
+	        });
+	        last_time = new Date().getTime();
+	        requestAnimationFrame(update);
+	    };
+	    update();
+	}
+
+	animate_start();
+
+	if (window.jQuery) {
+	    jQuery.fn.YohaAni = function (option, duration, cb) {
+	        for (var i = 0; i < this.length; i++) {
+	            Yoha.animate(this[i], option, duration).done(cb);
+	        }
+	    };
+	}
+
+/***/ }
+/******/ ]);
